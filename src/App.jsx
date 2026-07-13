dd632dd43a40f53d8aa0636869b3ed73756306b7
Muhammed Irfan S <irfanmhdm123@gmail.com>
2026-07-13 21:09:42 +0530

    Routes Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import View from './components/View'
+import Add from './components/Add'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+
+    <NavBar />
+    <Routes>
+
+      <Route path = "/add" element={<Add />}/>
+      <Route path = "/view" element={<View />}/>
+    </Routes>
+    
+    </BrowserRouter>
      
     </>
   )
